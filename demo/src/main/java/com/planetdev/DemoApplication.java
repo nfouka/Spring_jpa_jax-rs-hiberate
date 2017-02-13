b04f0bb74eb77f3eee7ca4666609c5a5c1241d42
nfouka <nadir.fouka@gmail.com>
2017-02-13 22:14:08 +0100

    modify configuration

--- demo/src/main/java/com/planetdev/DemoApplication.java
+++ demo/src/main/java/com/planetdev/DemoApplication.java
@@ -1,10 +1,20 @@
 package com.planetdev;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages={"com.planetdev"})
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class DemoApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
